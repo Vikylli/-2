9abd7aa46c679f69694db181e3a00b0df86bba8a
Vikylli <romasviktoria4@gmail.com>
2025-04-30 14:24:22 +0500

    9 практика

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -35,18 +35,30 @@ class PostController extends Controller
     public function store(Request $request)
     {
         $validatedData = $request->validate([
-            'title' => 'required',
-            'description' => 'required',
-            'content' => 'required',
-            'category_id' => 'required',
-            'tags' => 'required',
-            'thumbnail' => 'required',
-        ]);
-        $validatedData['views'] = 0;
-        $validatedData['thumbnail'] = 'default';
-        dd($validatedData);
-        Post::create($validatedData);
-        return to_route('posts.index')->with('success', 'Статья добавлена');
+            'title'=>'required',
+             'description'=>'required',
+             'content'=>'required',
+             'category_id'=>'required',
+             'tags'=>'required',
+             'category_id'=>'required|integer',
+             'yhumbnail'=>'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+         ]);
+ 
+         if ($request->hasFile('yhumbnail')) {
+             $image_name = time() . '-' .$request->image->extension();
+             $request->yhumbnail->move(public_path('images'), $image_name);
+             $path = 'storage/images/posts/' . $image_name;
+ 
+             $validatedData['yhumbnail'] = $path;
+             $folder = date('Y-m-d');
+             $validatedData['yhumbnail'] = $request->file('yhumbnail')->store("{images/$folder}");
+         }
+         $validatedData['views'] = 0;
+ 
+         Post::create($validatedData);
+         $post = Post::create($validatedData);
+         $post->tags()->sync($request->tags);
+         return to_route('posts.index', $validatedData) -> with('success', 'Статья успешно создана');
     }
 
     /**
